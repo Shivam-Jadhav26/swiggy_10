b4a16b49b88a36f5842f9e28ac48118fe6042ca4
Shivam Jadhav <shivamaj2005@gmail.com>
2026-02-14 19:55:31 +0530

    Update SearchFood.jsx

--- src/Components/SearchFood.jsx
+++ src/Components/SearchFood.jsx
@@ -15,8 +15,8 @@ export default function SearchFood(){
         
             async function fetchData() {
                
-               const proxyServer = "https://cors-anywhere.herokuapp.com/"
-               const swiggyAPI = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
+               //const proxyServer = "https://cors-anywhere.herokuapp.com/"
+               const swiggyAPI = `/api/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
                const response = await fetch(proxyServer+swiggyAPI);
                const data = await response.json();
                const tempData = data?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
